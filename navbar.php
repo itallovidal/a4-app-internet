33dbb73d1cd3853ef7d3264decfe4c15dc65505a
Gabriel Nathan <devgabrielnathan@gmail.com>
2025-04-25 18:19:01 -0300

    adding text

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
 <div id="navbar-wrapper">
     <nav id="top-navbar" class="content-wrapper-size">
-        <a class="text-regular btn-primary" href="index.php">Home</a>
-        <a class="text-regular btn-primary" href="">Sobre</a>
-        <a class="text-regular btn-primary" href="products.php">Sorvetes</a>
-        <a class="text-regular btn-primary" href="news.php">Novidades</a>
+        <a class="text-regular btn-primary" href="http://localhost/a4-app-internet/index.php">Home</a>
+        <a class="text-regular btn-primary" href="http://localhost/a4-app-internet/about.php">Sobre</a>
+        <a class="text-regular btn-primary" href="http://">Sorvetes</a>
+        <a class="text-regular btn-primary" href="http://">Novidades</a>
     </nav>
 </div>
