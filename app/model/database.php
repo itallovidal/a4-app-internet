c59243c41da72b80921a95a67b49f9fd0d0c7ea6
Leonardo P <leopatrosilva@gmail.com>
2025-05-08 12:08:58 -0300

    add port model database

--- app/model/database.php
+++ app/model/database.php
@@ -2,13 +2,14 @@
 
 class MySQLDatabase {
     private $host = "localhost";
+    private $port = "3306";
     private $database = "icecream_shop_db";
     private $user = "root";
     private $password = "";
 
     public function connect() {
         try {
-            $conn = new PDO("mysql:host=$this->host;dbname=$this->database", $this->user, $this->password);
+            $conn = new PDO("mysql:host=$this->host;port=$this->port;dbname=$this->database", $this->user, $this->password);
             return $conn;
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
