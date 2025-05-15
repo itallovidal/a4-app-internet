b1b02e2d019f53ef138f4d99b41a6c10d0ac41b3
Leonardo P <leopatrosilva@gmail.com>
2025-05-15 17:55:20 -0300

    separating management from the admin page, and configuring route

---
+++ app/views/partials/navbarAdmin.php
@@ -0,0 +1,8 @@
+<nav class="flex-column nav-admin flex-center">
+    <a class="text-regular btn-primary" href="<?php echo base_url('admin/products'); ?>">Produtos</a>
+    <a class="text-regular btn-primary" href="<?php echo base_url('admin/news'); ?>">Novidades</a>
+    <a class="text-regular btn-primary" href="<?php echo base_url('admin/users'); ?>">Admins</a>
+    <div>
+        <a class="text-regular btn-danger" href="">Sair</a>
+    </div>
+</nav>
