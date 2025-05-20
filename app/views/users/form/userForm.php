b71864b99095b855332cad092d04871862a3622a
Leonardo P <leopatrosilva@gmail.com>
2025-05-20 11:58:17 -0300

    add userForm style

--- app/views/users/form/userForm.php
+++ app/views/users/form/userForm.php
@@ -8,30 +8,36 @@
 </head>
 
 <body>
-    <?php include dirname(__DIR__, 2) . '/partials/navbar.php'; ?>
+    <?php include dirname(__DIR__, 2) . '/partials/navbarAdmin.php'; ?>
     <main>
-        <form class="content-wrapper-size" action="" method="POST">
+        <div class="form-container">
+            <header class="flex-row flex-center">
+                <i class="fa-solid fa-arrow-left"></i>
+                <h1 class="heading">Gerenciar Admin</h1>
+            </header>
+            <form class="content-wrapper-size flex-column flex-center" action="" method="POST">
             <div class="form-group">
                 <label for="name">Nome</label>
                 <input type="text" name="name" id="name" placeholder="Insira o nome do usuário" required>
             </div>
 
             <div class="form-group">
-                <label for="email">E-mail</label>   
-                <input type="text" name="email" id="email" placeholder="Insira o seu e-mail" required>
+                <label for="email">E-mail</label>
+                <input type="text" name="email" id="email" placeholder="Insira o e-mail" required>
             </div>
 
             <div class="form-group">
                 <label for="password">Senha</label>
-                <input type="password" name="password" id="password" placeholder="Insira sua senha" required minlength="8">
+                <input type="password" name="password" id="password" placeholder="Insira a senha" required minlength="8">
             </div>
             <div class="form-group">
                 <label for="password-confirmation">Confirmação de senha</label>
-                <input type="password" name="password-confirmation" id="password-confirmation" placeholder="Confirme sua senha" required minlength="8">
+                <input type="password" name="password-confirmation" id="password-confirmation" placeholder="Confirme a senha" required minlength="8">
             </div>
 
-            <button type="submit">Salvar</button>
+            <button type="submit" class="btn-primary">Salvar</button>
         </form>
+        </div>
     </main>
 </body>
 
