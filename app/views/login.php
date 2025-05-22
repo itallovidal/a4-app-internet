877d925a409730af758dbfa3f601189bf14ce3fb
itallovidal <itallovidalll@gmail.com>
2025-05-22 16:18:58 -0300

    fix height

--- app/views/login.php
+++ app/views/login.php
@@ -8,20 +8,19 @@
 </head>
 
 <body>
-    <?php include 'partials/navbar.php'; ?>
 
-    <main>
+    <main class="login-page flex-center">
+        <?php include 'partials/navbar.php'; ?>
         <form action="" method="post" class="content-wrapper-size">
             <div class="flex-column flex-center">
-                <h1 class="heading">Entrar</h1>
-                <p class="text-small">Entre com seu e-mail e senha para acessar sua conta.</p>
-                <p class="text-small" style="color: red;"><?php echo isset($_GET['expired']) ? 'Sessão expirada' : '';?></p>
-                <p class="text-small" style="color: red;"><?php echo isset($_GET['incorrect']) ? 'Email ou senha inválidos' : '';?></p>
+                <h1 class="heading">Painel Administrativo</h1>
+                <p class="text-small" style="color: red;"><?php echo isset($_GET['expired']) ? 'Sessão expirada' : ''; ?></p>
+                <p class="text-small" style="color: red;"><?php echo isset($_GET['incorrect']) ? 'Email ou senha inválidos' : ''; ?></p>
             </div>
             <div class="flex-column flex-center">
                 <input type="email" name="email" id="email" placeholder="Email" required>
                 <input type="password" name="password" id="password" placeholder="Senha" required>
-                <button class="btn-primary" type="submit">Entrar</button> 
+                <button class="btn-primary" type="submit">Entrar</button>
             </div>
         </form>
     </main>
