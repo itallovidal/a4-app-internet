0aa8cfa0cf6fb51535b2512ce1de984defba81e0
itallovidal <itallovidalll@gmail.com>
2025-05-22 16:17:07 -0300

    implemenet mobile navbar

--- app/views/partials/navbar.php
+++ app/views/partials/navbar.php
@@ -4,6 +4,16 @@
         <a class="text-regular btn-primary" href="<?php echo base_url('about'); ?>">Sobre</a>
         <a class="text-regular btn-primary" href="<?php echo base_url('products'); ?>">Sorvetes</a>
         <a class="text-regular btn-primary" href="<?php echo base_url('news'); ?>">Novidades</a>
-        <a class="text-regular btn-primary" id="login" href="<?php echo base_url('admin'); ?>">Entrar</a>
+        <a class="text-regular btn-primary" href="<?php echo base_url('admin'); ?>">Entrar</a>
+
+        <button class="close-mobile-nav btn-ghost flex-column" type="button">
+            <i class="fas fa-chevron-left"></i>
+        </button>
     </nav>
+</div>
+
+<div id="navbar-mobile" style="align-items: center;">
+    <button id="navbar-button-toggle" class="btn-primary flex-column" type="button">
+        <i class="fa-solid fa-bars"></i>
+    </button>
 </div>
