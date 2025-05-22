50ed15b03acb060a35cdf0224997d0c4ad8be202
itallovidal <itallovidalll@gmail.com>
2025-05-22 16:18:33 -0300

    fix navbar on top of the content

--- app/views/admin/news.php
+++ app/views/admin/news.php
@@ -2,56 +2,59 @@
 <html lang="pt-BR">
 
 <head>
-    <?php include dirname(__DIR__, 1) .'/partials/head.php'; ?>
+    <?php include dirname(__DIR__, 1) . '/partials/head.php'; ?>
     <link rel="stylesheet" href="<?= base_url('style/admin.css') ?>">
     <title>Página Admin</title>
 </head>
 
 <body>
-    <main>
+    <div class="admin-container">
         <?php include dirname(__DIR__, 1) . '/partials/navbarAdmin.php'; ?>
-        
-        <header class="content-wrapper-size admin">
-            <h1 class="heading">Bem vindo, <?php echo $_SESSION['user']['name']?>!</h1>
-            <p class="text-small">Esta é a página de administração do site.</p>
-            <p class="text-small">Você pode gerenciar as novidades na tabela abaixo</p>
-        </header>
+        <main>
 
-        <article class="content-wrapper-size admin">
-            <div class="flex-row flex-between">
-                <h2 class="heading">Gerenciar Novidades</h2>
-                <a class="btn-primary" href="<?php echo base_url('home'); ?>">Adicionar Novidade</a>
-            </div>
-            <p class="text-small">Adicione, edite ou remova novidades do site.</p>
-            <div class="table-wrapper">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Descrição</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($NewsList as $news): ?>
+            <header class="content-wrapper-size admin">
+                <h1 class="heading">Bem vindo, <?php echo $_SESSION['user']['name'] ?>!</h1>
+                <p class="text-small">Esta é a página de administração do site.</p>
+                <p class="text-small">Você pode gerenciar as novidades na tabela abaixo</p>
+            </header>
+
+            <article class="content-wrapper-size admin">
+                <div class="flex-row flex-between">
+                    <h2 class="heading">Gerenciar Novidades</h2>
+                    <a class="btn-primary" href="<?php echo base_url('home'); ?>">Adicionar Novidade</a>
+                </div>
+                <p class="text-small">Adicione, edite ou remova novidades do site.</p>
+                <div class="table-wrapper">
+                    <table>
+                        <thead>
                             <tr>
-                                <td><?= $news->getName() ?></td>
-                                <td><?= $news->getDescription() ?></td>
-                                <td class="flex-row flex-center">
-                                    <a href="<?php echo base_url('home'); ?>" class="btn-danger icon-button">
-                                        <i class="fas fa-trash-alt"></i>
-                                    </a>
-                                    <a href="<?php echo base_url('home'); ?>" class="icon-button">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                </td>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Ações</th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        </article>
-    </main>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($NewsList as $news): ?>
+                                <tr>
+                                    <td><?= $news->getName() ?></td>
+                                    <td><?= $news->getDescription() ?></td>
+                                    <td class="flex-row flex-center">
+                                        <a href="<?php echo base_url('home'); ?>" class="btn-danger icon-button">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </a>
+                                        <a href="<?php echo base_url('home'); ?>" class="icon-button">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </article>
+        </main>
+    </div>
+
 </body>
 
 </html>
--- app/views/admin/products.php
+++ app/views/admin/products.php
@@ -2,57 +2,60 @@
 <html lang="pt-BR">
 
 <head>
-    <?php include dirname(__DIR__, 1) .'/partials/head.php'; ?>
+    <?php include dirname(__DIR__, 1) . '/partials/head.php'; ?>
     <link rel="stylesheet" href="<?= base_url('style/admin.css') ?>">
     <title>Página Admin</title>
 </head>
 
 <body>
-    <main>
+    <div class="admin-container">
         <?php include dirname(__DIR__, 1) . '/partials/navbarAdmin.php'; ?>
-        
-        <header class="content-wrapper-size admin">
-            <h1 class="heading">Bem vindo, <?php echo $_SESSION['user']['name']?>!</h1>
-            <p class="text-small">Esta é a página de administração do site.</p>
-            <p class="text-small">Você pode gerenciar os produtos na tabela abaixo.</p>
-        </header>
-        <article class="content-wrapper-size admin">
-            <div class="flex-row flex-between">
-                <h2 class="heading">Gerenciar Produtos</h2>
-                <a class="btn-primary" href="<?php echo base_url('home'); ?>">Adicionar Produto</a>
-            </div>
-            <p class="text-small">Adicione, edite ou remova produtos do site.</p>
-            <div class="table-wrapper">
-                <table>
-                    <thead class="">
-                        <tr>
-                            <th>Nome</th>
-                            <th>Descrição</th>
-                            <th>Preço</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($icecreamList as $icecream): ?>
+
+        <main>
+            <header class="content-wrapper-size admin">
+                <h1 class="heading">Bem vindo, <?php echo $_SESSION['user']['name'] ?>!</h1>
+                <p class="text-small">Esta é a página de administração do site.</p>
+                <p class="text-small">Você pode gerenciar os produtos na tabela abaixo.</p>
+            </header>
+            <article class="content-wrapper-size admin">
+                <div class="flex-row flex-between">
+                    <h2 class="heading">Gerenciar Produtos</h2>
+                    <a class="btn-primary" href="<?php echo base_url('home'); ?>">Adicionar Produto</a>
+                </div>
+                <p class="text-small">Adicione, edite ou remova produtos do site.</p>
+                <div class="table-wrapper">
+                    <table>
+                        <thead class="">
                             <tr>
-                                <td><?= $icecream->getName() ?></td>
-                                <td><?= $icecream->getDescription() ?></td>
-                                <td>R$<?= $icecream->getPrice() ?></td>
-                                <td class="flex-row flex-center">
-                                    <a href="<?php echo base_url('home'); ?>" class="btn-danger icon-button">
-                                        <i class="fas fa-trash-alt"></i>
-                                    </a>
-                                    <a href="<?php echo base_url('home'); ?>" class="icon-button">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                </td>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Preço</th>
+                                <th>Ações</th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        </article>
-    </main>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($icecreamList as $icecream): ?>
+                                <tr>
+                                    <td><?= $icecream->getName() ?></td>
+                                    <td><?= $icecream->getDescription() ?></td>
+                                    <td>R$<?= $icecream->getPrice() ?></td>
+                                    <td class="flex-row flex-center">
+                                        <a href="<?php echo base_url('home'); ?>" class="btn-danger icon-button">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </a>
+                                        <a href="<?php echo base_url('home'); ?>" class="icon-button">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </article>
+        </main>
+
+    </div>
 </body>
 
 </html>
